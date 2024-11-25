328c876f4ba94e8882b14db6ff2ad34ce05df905
vats <vatsalmangukiya9003@gmail.com>
2024-11-25 20:39:55 +0530

    Remove next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-module.exports = {
-    experimental: {
-      appDir: true, // Indicates App Router is enabled
-    },
-  };
-  
+
 export default nextConfig;
