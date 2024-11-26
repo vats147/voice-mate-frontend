7146ed6c8f2565adc573af4e634347bb6459d4c5
vats <vatsalmangukiya9003@gmail.com>
2024-11-26 10:09:56 +0530

    Headaer updated

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -49,7 +49,7 @@ const Header = () => {
           ))}
         </nav>
 
-<<<<<<< HEAD
+
         {/* Login/Logout Button */}
         {isLoggedIn ? (
           <button
@@ -57,12 +57,7 @@ const Header = () => {
             className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800"
           >
             Logout
-=======
-        {/* Contact Us Button */}
-        <Link href="/login">
-          <button className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800">
-            Login
->>>>>>> 2733f99874be7ebaa4119d175a6d66ede26c1a33
+
           </button>
         ) : (
           <Link href="/login">
